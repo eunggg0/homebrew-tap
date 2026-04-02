023f93eed78fa5898a04fc318eddacec0f08ee8d
응 <eung@eung-ui-MacBookPro.local>
2026-04-02 16:00:49 +0900

    Bump to v1.1.0

--- Casks/claude-usage-monitor.rb
+++ Casks/claude-usage-monitor.rb
@@ -1,6 +1,6 @@
 cask "claude-usage-monitor" do
-  version "1.0.0"
-  sha256 "e9f8f72b06ecd4d62d80a3dd7d5e8a00a21a414232b6278f20af3a684bf2b364"
+  version "1.1.0"
+  sha256 "5ef9c8b1299b7bae63de0859237536075add1c799929ca5e0972a06f81b7cc21"
 
   url "https://github.com/eunggg0/homebrew-tap/releases/download/v#{version}/ClaudeUsageMonitor-#{version}.zip"
   name "Claude Usage Monitor"
